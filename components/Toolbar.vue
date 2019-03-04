c656ea37a22ebf97b24935ff1fce1f4ac9d9d1ce
Ivan Jakab <ivan.jakab@gauss.hr>
2019-03-04 09:34:16 +0100

    toolbar component, removed redundant code

---
+++ components/Toolbar.vue
@@ -0,0 +1,63 @@
+<template>
+   <div>
+     <v-navigation-drawer
+       v-model="drawer"
+       :mini-variant="miniVariant"
+       :clipped="clipped"
+       fixed
+       app
+     >
+       <v-list>
+         <v-list-tile
+           v-for="(item, i) in items"
+           :key="i"
+           :to="item.to"
+           router
+           exact
+         >
+           <v-list-tile-action>
+             <v-icon>{{ item.icon }}</v-icon>
+           </v-list-tile-action>
+           <v-list-tile-content>
+             <v-list-tile-title v-text="item.title" />
+           </v-list-tile-content>
+         </v-list-tile>
+       </v-list>
+     </v-navigation-drawer>
+     <v-toolbar
+       :clipped-left="clipped"
+       fixed
+       app
+     >
+       <v-toolbar-side-icon @click="drawer = !drawer" />
+       <v-toolbar-title v-text="title" />
+       <v-spacer />
+     </v-toolbar>
+   </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        clipped: false,
+        drawer: true,
+        fixed: false,
+        items: [
+          {
+            title: 'Petra Peric',
+            to: '/petra.peric'
+          },
+          {
+            title: 'Pero Peric',
+            to: '/pero.peric'
+          }
+        ],
+        miniVariant: false,
+        right: true,
+        rightDrawer: false,
+        title: 'Chat'
+      }
+    }
+  }
+</script>
