9eedf93b246e33b72f5960cf619e160eb2ee86c6
Ivan Jakab <ivan.jakab@gauss.hr>
2019-05-08 12:53:48 +0200

    env fix

--- env.example.js
+++ env.example.js
@@ -1,13 +1,9 @@
 export default {
   env: {
-    cookieDomain: 'localhost',
-    hostName: 'http://localhost:3000',
-    isProduction: false,
-    isProtected: false,
     subdomain: 'gaussdev',
     apiBaseUrl: 'https://apigaussbox.jsteam.gaussx.com',
     socketApiBaseUrl: 'https://apigaussbox.jsteam.gaussx.com/v1/chat/',
-    socketUrl: 'http://jsteam.gaussx.com:2132/',
+    socketUrl: 'ws://jsteam.gaussx.com:2132/',
   },
 
   server: {
