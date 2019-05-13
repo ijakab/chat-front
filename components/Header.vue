927d9bb3e64447381a82c6f75ae05a648ca1ebac
Ivan Jakab <ivan.jakab@gauss.hr>
2019-05-13 12:54:25 +0200

    display logged user

--- components/Header.vue
+++ components/Header.vue
@@ -1,11 +1,17 @@
 <template>
   <header>
-    <button class="btn btn-danger btn-sm" v-on:click="logout()">Logout</button>
+    {{user.displayName}}<button class="btn btn-danger btn-sm" v-on:click="logout()">Logout</button>
   </header>
 </template>
 
 <script>
   export default {
+    computed: {
+      user() {
+        return this.$auth.state.user.details
+      }
+    },
+
     methods: {
       async logout() {
         await this.$auth.logout()
